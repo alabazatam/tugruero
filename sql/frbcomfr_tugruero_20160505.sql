a1b28977cf2195d1b0b69c0c52bccbe133ceb43e
Javier <Javier@192.168.10.102>
2016-05-05 13:00:55 -0430

    page

--- sql/frbcomfr_tugruero_20160505.sql
+++ sql/frbcomfr_tugruero_20160505.sql
@@ -12,9 +12,8 @@ MySQL - 5.6.17 : Database - frbcomfr_tugruero
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`frbcomfr_tugruero` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
-USE `frbcomfr_tugruero`;
+USE `tugruerc_tugruero`;
 
 /*Table structure for table `apps` */
 
