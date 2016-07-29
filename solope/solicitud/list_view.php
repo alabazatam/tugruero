58cc198ba10c6a4f55f63e9cf01cd07d3a53b767
marcos <marcos@marcos-desktop>
2016-07-28 22:28:18 -0430

    google maps operadores

---
+++ solope/solicitud/list_view.php
@@ -0,0 +1,6 @@
+<?php include('../../view_header_app.php')?>
+<?php //include('../menu.php')?>
+
+	<a class="btn btn-default"  href="<?php echo full_url."/solope/solicitud/index.php?action=new"?>"><i class="fa fa-file-o fa-pull-left fa-border"></i>Agregar</a>
+
+	<?php include('../../view_footer.php')?>
