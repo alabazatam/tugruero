a588ed3037d7c5c29649c4a968db2b392845dbf1
marcos <marcos@marcos-desktop>
2017-03-31 21:11:46 -0430

    ventas online

--- web/js/mercadopago.js
+++ web/js/mercadopago.js
@@ -206,7 +206,7 @@ $(document).ready(function(){
                                             });   
                                         }else{
                                             
-                                                $('#show_error').html("<div class='alert alert-danger'>La transacción ha sido rechazada</div>");
+                                                $('#show_error').html("<div class='alert alert-danger'>La transacción ha sido rechazada" + status + "</div>");
 
                                             
                                             
