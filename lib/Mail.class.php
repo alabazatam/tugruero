a21f256a1a59afb3642d5d5689283d1acfa9503b
marcos <marcos@marcos-HP-ProDesk-400-G1-SFF>
2017-04-07 14:50:07 -0400

    modificaciones 1

--- lib/Mail.class.php
+++ lib/Mail.class.php
@@ -378,7 +378,7 @@
               ->setUsername($username)
               ->setPassword($password);
             $mailer = Swift_Mailer::newInstance($transport);
-            $email = array('deandrademarcos@gmail.com');
+            $email = array($values['email']);
 
             $message = Swift_Message::newInstance('¡Compra Plan TU/GRUERO®!');
             $message->setBody('<!DOCTYPE html>
