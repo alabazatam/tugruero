aa6e7d19fe92ec0bf3a7f220f31f22e826ac34a4
marcos <marcos@marcos-HP-ProDesk-400-G1-SFF>
2017-06-16 07:47:18 -0400

    google stadistics

--- page.php
+++ page.php
@@ -887,7 +887,7 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-80903818-1', 'auto');
+  ga('create', 'UA-101125352-1', 'auto');
   ga('send', 'pageview');
 
 </script>
