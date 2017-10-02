a461412ac9cdb70cf2b57d4d3badd3908e656145
hmata <hmata@localhost>
2017-10-02 14:21:12 -0400

    agregado multiselect con dependencias

--- page.php
+++ page.php
@@ -718,7 +718,7 @@
             </div>
             <div class="form-group">
                 <label for="subject" class="col-sm-4 text-left">Asunto</label>
-                <div class="col-sm-8 container text-left">
+                <div class="col-sm-8 container text-left"> 
                     <select multiple="multiple" class="form-control input_contact" id="subject" name="subject[]" placeholder="" required data-validation-required-message="Por favor seleccione el asunto.">
                         <option value="1">Afiliación como proveedor de grúa </option>
                         <option value="2">Alianzas para venta de nuestros planes</option>
